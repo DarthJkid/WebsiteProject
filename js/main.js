f1da836407d887ce3cc64411ecf4e4a177eaca52
DarthJkid <147732639+DarthJkid@users.noreply.github.com>
2024-04-03 12:12:02 +0000

    AOS install preliminary trial

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-import Swiper from 'swiper';
-
-let swiperCards = new Swiper('.card__content', {
-  loop: true,
-  spaceBetween: 32,
-  grabCursor: true,
-  clickable: true,
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+//initialise AOS animations
+AOS.init({
+duration: 1000,
+offset: 100,
 });
+
+import 'aos/dist/aos.css';
+import AOS from 'aos';
